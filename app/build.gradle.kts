0d8dd7770c60722f27854157c17ed97b470a74d8
fedderli <pokrzywnickikarol@gmail.com>
2025-12-10 20:12:24 +0100

    added files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidx.navigation.safeargs)
 }
 
 android {
@@ -35,6 +36,15 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    android{
+        buildFeatures {
+            viewBinding = true
+        }
+
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
 }
 
 dependencies {
@@ -46,4 +56,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
 }
---
+++ app/src/main/java/com/example/mobilne_kuchnia_tradycyjna/OrderViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.mobilne_kuchnia_tradycyjna
+
